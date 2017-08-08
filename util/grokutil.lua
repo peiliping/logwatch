ec30c64af83680b2d2e8395e623ceafc52039f3d
peiliping <peilipingplp@gmail.com>
2017-08-08 14:41:03 +0800

    u

--- util/grokutil.lua
+++ util/grokutil.lua
@@ -28,14 +28,17 @@ function grok.base(rule)
         t_grokstr , cts = string.gsub(t_grokstr , '%$([%a|_]+)%$([%a|_]+)' , function(l , f) return '$' .. l .. '_' .. f end)
     end
     local _ , count = string.gsub(t_grokstr , '%$([%a|_]+)' , '(.-)')
-    local index = 0 
+    local index , notail = 0 , false
     rule.regex = string.gsub(t_grokstr , '%$([%a|_]+)' , function(w) 
         index = index + 1
+        if index == count then
+            notail = (w == string.sub(t_grokstr , 0-string.len(w)))
+        end
         if w == 'ninja' then
-            return (index == count and '.*' or '.-')
+            return (notail and '.*' or '.-')
         else
             table.insert(rule.mapping , w)
-            return (index == count and '(.*)' or '(.-)')
+            return (notail and '(.*)' or '(.-)')
         end
     end)
 end
@@ -43,7 +46,6 @@ end
 function grok.plus(rule)
     local total_len , pos = string.len(rule.regex) , 0 
     local posSeq = {}
-    -- ensure the starting,ending of not greedy regex 
     while  total_len - pos > 4 do
         pos = string.find(rule.regex , '%(%.%-%)' , pos)
         if pos then
@@ -53,7 +55,6 @@ function grok.plus(rule)
             pos = total_len
         end
     end
-    -- convert not greedy regex to limit greey regex
     local index = 1
     local result = string.gsub(rule.regex , '%(%.%-%)' , function(w)
         local sp = string.sub(rule.regex , posSeq[index] , posSeq[index])
@@ -64,11 +65,9 @@ function grok.plus(rule)
         index = index + 1
         return sp
     end)
-    -- ignore multi blank split
     if rule.easysplit then
         result = string.gsub(result , '%)%s+' , ')%%s+')
     end
-
     rule.regex = result
 end
 
